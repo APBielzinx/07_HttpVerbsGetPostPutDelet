fd3f6181834b05e65e63eba3fe9295bdbfc3b275
APBielzinx <112442554+APBielzinx@users.noreply.github.com>
2023-04-11 17:08:34 -0300

    FindAll

--- api-getway/src/main/java/br/com/erudio/PersonController.java
+++ api-getway/src/main/java/br/com/erudio/PersonController.java
@@ -9,6 +9,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.awt.*;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
@@ -20,14 +21,24 @@ public class PersonController {
 
     @RequestMapping(value = "/{id}",
             method = RequestMethod.GET,
-    produces = MediaType.APPLICATION_JSON_VALUE)
+            produces = MediaType.APPLICATION_JSON_VALUE)
 
     //soma
-    public Person findById(@PathVariable(value = "id") String id) throws Exception{
-
+    public Person findById(@PathVariable(value = "id") String id) throws Exception {
 
 
         return services.findById(id);
 
+    }
+
+    @RequestMapping(
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+
+
+    public List<Person> findAll() {
+
+
+        return services.findAll();
     }
 }
