323d96e9135bdd82234261a806e7d5dc5e2c3c38
APBielzinx <112442554+APBielzinx@users.noreply.github.com>
2023-04-11 18:05:04 -0300

    fim da sessao

--- api-getway/src/main/java/br/com/erudio/PersonController.java
+++ api-getway/src/main/java/br/com/erudio/PersonController.java
@@ -1,20 +1,22 @@
 package br.com.erudio;
 
-import br.com.erudio.exceptions.UnsuportedMathOperationException;
+
 
 import br.com.erudio.model.Person;
 import br.com.erudio.services.PersonServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
-
-import java.awt.*;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
+
 
 @RestController
 @RequestMapping("/person")
 public class PersonController {
+
 
     @Autowired
     private PersonServices services;
@@ -23,13 +25,41 @@ public class PersonController {
             method = RequestMethod.GET,
             produces = MediaType.APPLICATION_JSON_VALUE)
 
-    //soma
+
     public Person findById(@PathVariable(value = "id") String id) throws Exception {
 
 
         return services.findById(id);
 
     }
+
+    @RequestMapping(
+            method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+
+
+    public Person create(@RequestBody Person person) throws Exception {
+
+
+        return services.create(person);
+
+    }
+
+    @RequestMapping(
+            method = RequestMethod.PUT,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+
+
+    public Person update(@RequestBody Person person) throws Exception {
+
+
+        return services.update(person);
+
+    }
+
+
 
     @RequestMapping(
             method = RequestMethod.GET,
@@ -40,5 +70,15 @@ public class PersonController {
 
 
         return services.findAll();
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE)
+
+    public void delete(@RequestBody  String id) throws Exception {
+
+
+    services.delete(id);
+
+     
     }
 }
--- api-getway/src/main/java/br/com/erudio/services/PersonServices.java
+++ api-getway/src/main/java/br/com/erudio/services/PersonServices.java
@@ -42,6 +42,21 @@ public class PersonServices {
 
     }
 
+
+    public Person update(Person person) {
+        logger.info("Updating one person");
+        return person;
+    }
+    public Person create(Person person) {
+        logger.info("Creating one person");
+        return person;
+    }
+
+    public void delete(String id) {
+        logger.info("deleting one person");
+
+    }
+
     private Person mockPerson(int i) {
         Person person = new Person();
         person.setId(counter.incrementAndGet());
